afaed84a16a39eea7add08516a43d9389a0b9051
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2022-05-05 19:48:24 -0700

    waytoodank

---
+++ Assets/VRCAvatars3Tools/AnimatorControllerCombiner/Editor/AnimatorControllerCombiner.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using AnimatorController = UnityEditor.Animations.AnimatorController;
+using Gatosyocora.VRCAvatars3Tools.Utilitys;
+
+// ver 1.1
+// Copyright (c) 2020 gatosyocora
+// MIT License. See LICENSE.txt
+
+namespace Gatosyocora.VRCAvatars3Tools
+{
+    public class AnimatorControllerCombiner : EditorWindow
+    {
+        private AnimatorController srcController;
+        private AnimatorController dstController;
+
+        private bool[] isCopyLayers;
+        private bool[] isCopyParameters;
+
+        private Vector2 srcControllerScrollPos = Vector2.zero;
+        private Vector2 dstControllerScrollPos = Vector2.zero;
+
+        [MenuItem("VRCAvatars3Tools/AnimatorControllerCombiner")]
+        public static void Open()
+        {
+            GetWindow<AnimatorControllerCombiner>("AnimatorControllerCombiner");
+        }
+
+        private void OnGUI()
+        {
+            using (var check = new EditorGUI.ChangeCheckScope())
+            {
+                srcController = EditorGUILayout.ObjectField("Src AnimatorController", srcController, typeof(AnimatorController), true) as AnimatorController;
+                if (check.changed)
+                {
+                    if (srcController != null)
+                    {
+                        isCopyLayers = Enumerable.Range(1, srcController.layers.Length)
+                                            .Select(i => true)
+                                            .ToArray();
+                        isCopyParameters = Enumerable.Range(1, srcController.parameters.Length)
+                                                .Select(i => true)
+                                                .ToArray();
+                    }
+                }
+            }
+            if (srcController != null)
+            {
+                using (new EditorGUI.IndentLevelScope())
+                using (var scroll = new EditorGUILayout.ScrollViewScope(srcControllerScrollPos, new GUIStyle(), new GUIStyle("verticalScrollbar")))
+                {
+                    srcControllerScrollPos = scroll.scrollPosition;
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        using (new EditorGUILayout.VerticalScope())
+                        {
+                            EditorGUILayout.LabelField("Layers", EditorStyles.boldLabel);
+                            for (int i = 0; i < srcController.layers.Length; i++)
+                            {
+                                var layer = srcController.layers[i];
+                                using (new EditorGUILayout.HorizontalScope())
+                                {
+                                    isCopyLayers[i] = EditorGUILayout.ToggleLeft(layer.name, isCopyLayers[i]);
+                                }
+                            }
+                        }
+                        GUILayout.FlexibleSpace();
+                        using (new EditorGUILayout.VerticalScope())
+                        {
+                            EditorGUILayout.LabelField("Parameters", EditorStyles.boldLabel);
+                            for (int i = 0; i < srcController.parameters.Length; i++)
+                            {
+                                var parameter = srcController.parameters[i];
+                                using (new EditorGUILayout.HorizontalScope())
+                                {
+                                    isCopyParameters[i] = EditorGUILayout.ToggleLeft($"[{parameter.type}]{parameter.name}", isCopyParameters[i]);
+                                }
+                            }
+                        }
+                    }
+                    EditorGUILayout.Space();
+                }
+            }
+
+            EditorGUILayout.Space();
+            using (new EditorGUI.IndentLevelScope())
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+                EditorGUILayout.LabelField("Copy ↓", GUILayout.Width(60f));
+                GUILayout.FlexibleSpace();
+            }
+            EditorGUILayout.Space();
+
+            dstController = EditorGUILayout.ObjectField("Dst AnimatorController", dstController, typeof(AnimatorController), true) as AnimatorController;
+            if (dstController != null)
+            {
+                using (new EditorGUI.IndentLevelScope())
+                using (var scroll = new EditorGUILayout.ScrollViewScope(dstControllerScrollPos, new GUIStyle(), new GUIStyle("verticalScrollbar")))
+                {
+                    dstControllerScrollPos = scroll.scrollPosition;
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        using (new EditorGUILayout.VerticalScope())
+                        {
+                            EditorGUILayout.LabelField("Layers", EditorStyles.boldLabel);
+                            foreach (var layer in dstController.layers)
+                            {
+                                using (new EditorGUILayout.HorizontalScope())
+                                {
+                                    EditorGUILayout.LabelField(layer.name);
+                                }
+                            }
+                        }
+                        GUILayout.FlexibleSpace();
+                        using (new EditorGUILayout.VerticalScope())
+                        {
+                            EditorGUILayout.LabelField("Parameters", EditorStyles.boldLabel);
+                            foreach (var parameter in dstController.parameters)
+                            {
+                                using (new EditorGUILayout.HorizontalScope())
+                                {
+                                    EditorGUILayout.LabelField($"[{parameter.type}]{parameter.name}");
+                                }
+                            }
+                        }
+                    }
+                    EditorGUILayout.Space();
+                }
+            }
+
+            EditorGUILayout.Space();
+
+            using (new EditorGUI.DisabledGroupScope(!srcController || !dstController))
+            {
+                if (GUILayout.Button("Combine"))
+                {
+                    for (int i = 0; i < srcController.layers.Length; i++)
+                    {
+                        if (!isCopyLayers[i]) continue;
+                        AnimatorControllerUtility.AddLayer(dstController, srcController.layers[i], i == 0);
+                    }
+
+                    for (int i = 0; i < srcController.parameters.Length; i++)
+                    {
+                        if (!isCopyParameters[i]) continue;
+                        AnimatorControllerUtility.AddParameter(dstController, srcController.parameters[i]);
+                    }
+                }
+            }
+
+            EditorGUILayout.Space();
+        }
+    }
+}
---
+++ Assets/VRCAvatars3Tools/VRCAssetCreator/Editor/VRCAssetCreator.cs
@@ -0,0 +1,108 @@
+﻿using System.IO;
+using UnityEditor;
+using Gatosyocora.VRCAvatars3Tools.Utilitys;
+using UnityEditor.Animations;
+
+// ver 1.1
+// Copyright (c) 2020 gatosyocora
+// MIT License. See LICENSE.txt
+
+namespace Gatosyocora.VRCAvatars3Tools
+{
+    public class VRCAssetCreator : Editor
+    {
+        [MenuItem("Assets/Create/VRChat/Controllers/ActionLayer", priority = 4)]
+        public static AnimatorController CreateActionLayer()
+             => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3ActionLayer");
+
+        [MenuItem("Assets/Create/VRChat/Controllers/FaceLayer", priority = 4)]
+        public static AnimatorController CreateFaceLayer()
+            => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3FaceLayer");
+
+        [MenuItem("Assets/Create/VRChat/Controllers/HandsLayer", priority = 5)]
+        public static AnimatorController CreateHandsLayer()
+            => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3HandsLayer");
+
+        [MenuItem("Assets/Create/VRChat/Controllers/HandsLayer2", priority = 5)]
+        public static AnimatorController CreateHandsLayer2()
+            => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3HandsLayer2");
+
+        [MenuItem("Assets/Create/VRChat/Controllers/IdleLayer", priority = 2)]
+        public static AnimatorController CreateIdleLayer()
+            => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3IdleLayer");
+
+        [MenuItem("Assets/Create/VRChat/Controllers/LocomotionLayer", priority = 1)]
+        public static AnimatorController CreateLocomotionLayer()
+            => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3LocomotionLayer");
+
+        [MenuItem("Assets/Create/VRChat/Controllers/SittingLayer", priority = 16)]
+        public static AnimatorController CreateSittingLayer()
+            => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3SittingLayer");
+
+        [MenuItem("Assets/Create/VRChat/Controllers/SittingLayer2", priority = 16)]
+        public static AnimatorController CreateSittingLayer2()
+            => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3SittingLayer2");
+
+        [MenuItem("Assets/Create/VRChat/Controllers/UtilityIKPose", priority = 18)]
+        public static AnimatorController CreateUtilityIKPose()
+            => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3UtilityIKPose");
+
+        [MenuItem("Assets/Create/VRChat/Controllers/UtilityTPose", priority = 17)]
+        public static AnimatorController CreateUtilityTPose()
+            => DuplicateVRCAsset<AnimatorController>("vrc_AvatarV3UtilityTPose");
+
+        [MenuItem("Assets/Create/VRChat/BlendTrees/New BlendTree", priority = 0)]
+        public static BlendTree CreateNewBlendTree()
+            => CreateNewBlendTree("new BlendTree");
+
+        [MenuItem("Assets/Create/VRChat/BlendTrees/StandingLocomotion", priority = 1)]
+        public static BlendTree CreateStandingLocomotionBlendTree()
+            => DuplicateVRCAsset<BlendTree>("vrc_StandingLocomotion");
+
+        [MenuItem("Assets/Create/VRChat/BlendTrees/CrouchingLocomotion", priority = 2)]
+        public static BlendTree CreateCrouchingLocomotionBlendTree()
+            => DuplicateVRCAsset<BlendTree>("vrc_CrouchingLocomotion");
+
+        [MenuItem("Assets/Create/VRChat/BlendTrees/ProneLocomotion", priority = 3)]
+        public static BlendTree CreateProneLocomotionBlendTree()
+            => DuplicateVRCAsset<BlendTree>("vrc_ProneLocomotion");
+
+        private static BlendTree CreateNewBlendTree(string assetName)
+        {
+            var folder = Selection.activeObject;
+            var folderPath = AssetDatabase.GetAssetPath(folder);
+            var extention = ".asset";
+            var assetPath = AssetDatabase.GenerateUniqueAssetPath(Path.Combine(folderPath, $"{assetName}{extention}"));
+
+            var asset = new BlendTree 
+            {
+                name = Path.GetFileNameWithoutExtension(assetPath)
+            };
+            AssetDatabase.CreateAsset(asset, assetPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            Selection.activeObject = asset;
+            return asset;
+        }
+
+        private static T DuplicateVRCAsset<T>(string assetName) where T : UnityEngine.Object
+        {
+            var baseAssetPath = VRCAssetUtility.GetVRCAssetPathForSearch($"{assetName} t:{typeof(T).Name}");
+
+            var folder = Selection.activeObject;
+            var folderPath = AssetDatabase.GetAssetPath(folder);
+            var extention = Path.GetExtension(baseAssetPath);
+            var assetPath = AssetDatabase.GenerateUniqueAssetPath(Path.Combine(folderPath, $"{assetName}{extention}"));
+            AssetDatabase.CopyAsset(baseAssetPath, assetPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            var asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+            Selection.activeObject = asset;
+            return asset;
+        }
+    }
+}
+
+
